a65af5ab4350c737f4dc4ea009182984d9eaadf2
Stanley Hughes <stan.hughes@riverbed.com>
2018-04-09 09:02:54 -0700

    Added profile lisitng and creating feature.

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -6,6 +6,7 @@ from rest_framework import status
 from rest_framework import viewsets
 
 from . import serializers
+from . import models
 
 # Create your views here.
 
@@ -102,3 +103,9 @@ class HelloViewSet(viewsets.ViewSet):
         """Handles deleting an object."""
 
         return Response({'http_method': 'DELETE'})
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handles creating, creating and updating profiles."""
+
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
