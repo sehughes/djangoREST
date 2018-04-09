3cfba4f899a2e0a1fe87b0aef45a7b9c9c332fa1
Stanley Hughes <stan.hughes@riverbed.com>
2018-04-09 12:38:11 -0700

    updates to chnage project path

--- src/profiles_project/deploy/server_setup.sh
+++ src/profiles_project/deploy/server_setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # TODO: Set to URL of git repo.
-PROJECT_GIT_URL='https://github.com/sehughes/profiles-rest-api.git'
+PROJECT_GIT_URL='https://github.com/sehughes/djangoREST.git'
 
 PROJECT_BASE_PATH='/usr/local/apps'
 VIRTUALENV_BASE_PATH='/usr/local/virtualenvs'
@@ -18,25 +18,25 @@ pip install --upgrade pip
 pip install virtualenv
 
 mkdir -p $PROJECT_BASE_PATH
-git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH/profiles-rest-api
+git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH/djangoREST
 
 mkdir -p $VIRTUALENV_BASE_PATH
 virtualenv --python=python3 $VIRTUALENV_BASE_PATH/profiles_api
 
 source $VIRTUALENV_BASE_PATH/profiles_api/bin/activate
-pip install -r $PROJECT_BASE_PATH/profiles-rest-api/requirements.txt
+pip install -r $PROJECT_BASE_PATH/djangoREST/requirements.txt
 
 # Run migrations
-cd $PROJECT_BASE_PATH/profiles-rest-api/src
+cd $PROJECT_BASE_PATH/djangoREST/src
 
 # Setup Supervisor to run our uwsgi process.
-cp $PROJECT_BASE_PATH/profiles-rest-api/deploy/supervisor_profiles_api.conf /etc/supervisor/conf.d/profiles_api.conf
+cp $PROJECT_BASE_PATH/djangoREST/deploy/supervisor_profiles_api.conf /etc/supervisor/conf.d/profiles_api.conf
 supervisorctl reread
 supervisorctl update
 supervisorctl restart profiles_api
 
 # Setup nginx to make our application accessible.
-cp $PROJECT_BASE_PATH/profiles-rest-api/deploy/nginx_profiles_api.conf /etc/nginx/sites-available/profiles_api.conf
+cp $PROJECT_BASE_PATH/djangoREST/deploy/nginx_profiles_api.conf /etc/nginx/sites-available/profiles_api.conf
 rm /etc/nginx/sites-enabled/default
 ln -s /etc/nginx/sites-available/profiles_api.conf /etc/nginx/sites-enabled/profiles_api.conf
 systemctl restart nginx.service
